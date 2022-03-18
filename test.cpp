5bd2129c08216bd71a64862fa68d925e33bd7633
Bui Dinh Van <buidinhvan98@gmail.com>
2022-03-18 13:56:28 +0700

    resolve in man hinh

--- test.cpp
+++ test.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
     int a = 2, b = 3;
     a = a + b;
-    cout << "Khong hien thi: " << "a" << endl;
+    cout << "Khong hien thi: " << a << endl;
     return 0;
 }
